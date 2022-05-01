89eb85a39e36c5a6e986c800901da0d3b0f103e0
kirua <yo44prg@icloud.com>
2022-05-02 01:19:04 +0200

    feat: add js doc for css minifier and minifier bin file and fix problem with eslint

--- src/minifier-css/minifierCss.js
+++ src/minifier-css/minifierCss.js
@@ -1,6 +1,11 @@
 const fs = require('fs');
 const logger = require('pino')();
 
+/**
+ * read css file with file path
+ * @param {string} filesPath path of css file
+ * @returns {string} return cotent in file
+ */
 function readCssFile(filesPath) {
   try {
     return fs.readFileSync(filesPath, 'utf8');
@@ -10,11 +15,21 @@ function readCssFile(filesPath) {
   }
 }
 
+/**
+ * minify css content
+ * @param {string} cssContent content in css file
+ * @returns {string} return css content minify
+ */
 function cssFormatter(cssContent) {
   const regex = /(\sr\n|\n|\r)/gm;
   return cssContent.replace(regex, '').split(' ').filter((elmnt) => elmnt !== '').join('');
 }
 
+/**
+ * create new file.min.css
+ * @param {string} cssMinify css content minify
+ * @param {*} filePathAndName path and name of file
+ */
 function createCssMinFile(cssMinify, filePathAndName) {
   try {
     fs.writeFileSync(`${filePathAndName}.min.css`, cssMinify);
@@ -23,6 +38,9 @@ function createCssMinFile(cssMinify, filePathAndName) {
   }
 }
 
+/**
+ * sample css minifier
+ */
 const cssMinifierSample = () => {
   const filesPath = './resources/css/index.css';
   const css = readCssFile(filesPath);
@@ -31,12 +49,21 @@ const cssMinifierSample = () => {
   createCssMinFile(cssMinify);
 };
 
+/**
+ * extract path and name of css file
+ * @param {string} path path of css file
+ * @returns {string} path and name of css file without extension
+ */
 const extractFileName = (path) => {
   const pathSplited = path.split('.');
   pathSplited.pop();
   return pathSplited[0];
 };
 
+/**
+ * chore function of css minifier
+ * @param {Array} cssFilesPath path of css files in project
+ */
 const cssMinifier = (cssFilesPath) => {
   cssFilesPath.forEach((filePath) => {
     const filePathAndName = extractFileName(filePath);
