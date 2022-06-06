663758c2a3b92752a281e9781474cd542fc3e287
kirua <yo44prg@icloud.com>
2022-06-06 11:54:05 +0200

    feat: prepare code for added configuration files options manager

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const { cssMinifier } = require('./src/minifier-css');
 const { htmlMinifier } = require('./src/minifier-html');
 const { jsMinifier, tsMinifier } = require('./src/minifier-js');
+const { manageDistOptions, manageLanguageOptions, manageExcludeOptions } = require('./src/configFiles');
+
+const manageConfigOptions = (configOptions) => {
+  if ('dist' in configOptions) {
+    manageDistOptions();
+  }
+
+  if ('language' in configOptions) {
+    manageLanguageOptions(configOptions.language);
+  }
+
+  if ('exclude' in configOptions) {
+    manageExcludeOptions(configOptions.exclude);
+  }
+};
 
 const minifierCommandLine = (args) => {
   if (args.config) {
-    console.log(args.config);
+    manageConfigOptions(args.config);
   } else {
     if (args.css) {
       cssMinifier(args.cssPath);
